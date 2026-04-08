efe814f32396f3a419c6c76a97b6586cb3f08050
Noah Hefner <nhefner127+github@protonmail.com>
2026-04-08 08:05:42 -0400

    work

--- repo_add.py
+++ repo_add.py
@@ -1,20 +1,74 @@
+#!/usr/bin/env python3
+
+from tarfile import TarFile
+import sys
+from pathlib import Path
+from argparse import Namespace
 import argparse
+import tempfile
+import tarfile
+
+def prepare_repo_db():
+
+    """
+    acquire file lock on lock file
+      - use retry logic
+
+    if db exists
+        if db is empty
+            error out
+        verify signature
+        extract db to temp dir
+
+    """
+
+    pass
+
+def add(db: Path):
+
+    try:
+        tar: TarFile = tarfile.open(db)
+        tar.extract(".PKGINFO")
+    except tarfile.ReadError as e:
+        # TODO: Cleanup files?
+        print(f"Failed to read database archive: {e}")
+        sys.exit(1)
+    except Exception as e:
+        # TODO: Cleanup files?
+        print(f"Unexpected error: {e}")
+        sys.exit(1)
 
 def main():
 
+    # parse program arguments
     parser = argparse.ArgumentParser(
         prog='pypac',
         description='Arch package manager, written in Python'
     )
-
     parser.add_argument("db_path")
-
     parser.add_argument("package_name")
-
     args: Namespace = parser.parse_args()
 
-    print(args)
+    # create Path object for database archive
+    db = Path(args.db_path)
 
+    # check that the database archive exists
+    if not db.exists():
+        print(f"{str(db)} does not exist.")
+        exit(1)
 
+    # create a temp dir for database building
+    tmp_dir = Path(tempfile.mkdtemp(prefix="repo-tools."))
+    db_dir: Path = tmp_dir / "db"
+    files_dir: Path = tmp_dir / "files"
+    db_dir.mkdir()
+    files_dir.mkdir()
+
+    # TODO: Verify PGP signature
+
+    # TODO: Prep database
+
+    # add()
+    
 if __name__ == "__main__":
     main()
